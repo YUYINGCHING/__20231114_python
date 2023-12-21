d279d7d661c8f44969036b0813a600a9704d7b03
YUYINGCHING <150818774+YUYINGCHING@users.noreply.github.com>
2023-12-21 14:00:13 +0000

    modify

--- lesson12_1/tools.py
+++ lesson12_1/tools.py
@@ -1,18 +1,32 @@
+from random import randint,choices
 import csv
+from csv import DictWriter
 
+def getStudents(nums:int) -> list[dict]:  
+    students:list[dict] = []
+    with open('names.txt',mode='r',encoding='utf-8') as file:
+        names:str = file.read()
+    nameList:list[str] = names.split('\n')
+    names:list[str] = choices(nameList,k=nums)
 
+    for i in range(nums):
+        stu = {
+        '姓名':names[i],
+        '國文':randint(45,100),
+        '英文':randint(45,100),
+        '數學':randint(45,100),
+        '地理':randint(45,100),
+        '歷史':randint(45,100),
+        }
+        students.append(stu)
+        
+    return students
 
-def saveToCSV(fileName:str,data:list[list],subject_nums:int) -> bool:
-    fileName += ".csv"
-    subjects = [f'科目{i+1}' for i in range(subject_nums)]
-    fields = ['姓名']
-    fields.extend(subjects)
-    with open(fileName,mode='w',encoding='utf-8',newline='') as file:
-        try:
-            writer = csv.writer(file)
-            writer.writerow(fields)
-            writer.writerows(data)
-        except:
-            return False
-        else:
-            return True
+def save_to_csv(students:list[dict],fileName:str)->None:    
+    fileNameWithExtension:str = fileName + '.csv'
+    with open(fileNameWithExtension,mode='w',encoding='utf-8',newline='') as file:
+        fieldnames:list[str] = ['姓名', '國文', '英文', '數學', '地理', '歷史']
+        writer:DictWriter = csv.DictWriter(file,fieldnames=fieldnames)
+        writer.writeheader()
+        writer.writerows(students)
+    print("寫入成功")
